360a924eadaa8d0a0ed6236f14febad05d00b62e
fionamei <fionamei131@gmail.com>
2022-06-16 16:35:22 -0700

    can upload prof pic but not yet saved to parse

--- Instagram/app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
+++ Instagram/app/src/main/java/com/example/instagram/fragments/ProfileFragment.java
@@ -1,21 +1,40 @@
 package com.example.instagram.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ImageDecoder;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.instagram.PostFavoriteListener;
 import com.example.instagram.R;
 import com.example.instagram.adapters.PostsAdapter;
 import com.example.instagram.adapters.ProfilePostsAdapter;
@@ -25,7 +44,10 @@ import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,6 +61,8 @@ public class ProfileFragment extends Fragment {
     protected ProfilePostsAdapter adapter;
     protected List<Post> allPosts;
     public static final String TAG = "ProfileFragment";
+    // PICK_PHOTO_CODE is a constant integer
+    public final static int PICK_PHOTO_CODE = 1046;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -59,6 +83,12 @@ public class ProfileFragment extends Fragment {
         user = ParseUser.getCurrentUser();
         populateViews();
         queryPosts();
+        ivProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickPhoto(v);
+            }
+        });
 
     }
 
@@ -78,21 +108,13 @@ public class ProfileFragment extends Fragment {
         tvUsername.setText(user.getUsername());
         rvPosts.setLayoutManager(new GridLayoutManager(getContext(), NUM_COLS));
         rvPosts.setAdapter(adapter);
-
     }
 
     private void queryPosts() {
-//        int POST_LIMIT = 20;
-        // specify type of data - post.class
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        // include data referred by user key
         query.include(Post.KEY_USER);
-        // limit to only 20 items
-//        query.setLimit(POST_LIMIT);
-        // order by creation date, newest first
         query.whereEqualTo("user", user);
         query.addDescendingOrder("createdAt");
-        // start async call for posts
         query.findInBackground(new FindCallback<Post>() {
             @Override
             public void done(List<Post> posts, ParseException e) {
@@ -100,9 +122,72 @@ public class ProfileFragment extends Fragment {
                     Log.e(TAG, "issue with getting posts", e);
                     return;
                 }
-
                 allPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    // Trigger gallery selection for a photo
+    public void onPickPhoto(View view) {
+        // Create intent for picking a photo from the gallery
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            // Bring up gallery to select a photo
+            galleryLauncher.launch(intent);
+        }
+    }
+
+    public Bitmap loadFromUri(Uri photoUri) {
+        Bitmap image = null;
+        try {
+            // check version of Android on device
+            if(Build.VERSION.SDK_INT > 27){
+                // on newer versions of Android, use the new decodeBitmap method
+                ImageDecoder.Source source = ImageDecoder.createSource(getContext().getContentResolver(), photoUri);
+                image = ImageDecoder.decodeBitmap(source);
+            } else {
+                // support older versions of Android by using getBitmap
+                image = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Uri photoUri = data.getData();
+                        Bitmap selectedImage = loadFromUri(photoUri);
+                        ivProfilePic.setImageBitmap(selectedImage);
+//                        save(user, selectedImage);
+                    }
+                }
+            }
+    );
+
+    private void save(ParseUser currentUser, File photoFile) {
+        currentUser.put("profilePic", photoFile);
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "error while saving", e);
+                    Toast.makeText(getContext(), "Error while saving", Toast.LENGTH_SHORT).show();
+                } else {
+//                    etDescription.setText("");
+//                    ivPostImage.setImageResource(0);
+                }
             }
         });
     }
