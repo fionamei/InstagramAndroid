019df40900e2832cbee93f9c6cc21e522e53fa32
fionamei <fionamei131@gmail.com>
2022-06-17 09:52:59 -0700

    endless scroll view implemented

--- Instagram/app/src/main/java/com/example/instagram/fragments/FeedFragment.java
+++ Instagram/app/src/main/java/com/example/instagram/fragments/FeedFragment.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.instagram.EndlessRecyclerViewScrollListener;
 import com.example.instagram.models.Post;
 import com.example.instagram.adapters.PostsAdapter;
 import com.example.instagram.R;
@@ -22,6 +23,7 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class FeedFragment extends Fragment {
@@ -31,7 +33,8 @@ public class FeedFragment extends Fragment {
     protected List<Post> allPosts;
     public static final String TAG = "FeedActivity";
     private SwipeRefreshLayout swipeContainer;
-
+    private EndlessRecyclerViewScrollListener scrollListener;
+    private Date lastPostDate;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -55,15 +58,21 @@ public class FeedFragment extends Fragment {
         adapter = new PostsAdapter(getContext(), allPosts);
 
         rvPosts.setAdapter(adapter);
-        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvPosts.setLayoutManager(linearLayoutManager);
 
         queryPosts();
         swipeListener();
+        scrollListener(linearLayoutManager);
+        // Adds the scroll listener to RecyclerView
+        rvPosts.addOnScrollListener(scrollListener);
 
     }
 
+
+
     private void queryPosts() {
-        int POST_LIMIT = 20;
+        int POST_LIMIT = 5;
         // specify type of data - post.class
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         // include data referred by user key
@@ -80,9 +89,9 @@ public class FeedFragment extends Fragment {
                     Log.e(TAG, "issue with getting posts", e);
                     return;
                 }
-
                 allPosts.addAll(posts);
                 adapter.notifyDataSetChanged();
+                lastPostDate= allPosts.get(POST_LIMIT-1).getCreatedAt();
             }
         });
     }
@@ -101,5 +110,41 @@ public class FeedFragment extends Fragment {
         adapter.clear();
         adapter.addAll(allPosts);
         swipeContainer.setRefreshing(false);
+    }
+
+    private void scrollListener(LinearLayoutManager linearLayoutManager) {
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                loadNextDataFromApi(lastPostDate);
+            }
+        };
+    }
+
+    private void loadNextDataFromApi(Date date) {
+        loadMorePosts(date);
+    }
+
+    private void loadMorePosts(Date createdAt) {
+        int POST_LIMIT = 5;
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(POST_LIMIT);
+        query.addDescendingOrder("createdAt");
+        query.whereLessThan("createdAt", createdAt);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "issue with getting posts", e);
+                    return;
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+                lastPostDate= allPosts.get(allPosts.size()-1).getCreatedAt();
+            }
+        });
     }
 }
